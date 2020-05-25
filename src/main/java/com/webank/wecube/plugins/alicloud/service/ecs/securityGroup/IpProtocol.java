e25c02069e33dabdb2bb441cd1d418ee8d789a35
HoweChen <yuhao.howe.chen@gmail.com>
2020-05-25 21:10:39 +0800

    #166 add IpProtocol enum

---
+++ src/main/java/com/webank/wecube/plugins/alicloud/service/ecs/securityGroup/IpProtocol.java
@@ -0,0 +1,17 @@
+package com.webank.wecube.plugins.alicloud.service.ecs.securityGroup;
+
+/**
+ * @author howechen
+ */
+public enum IpProtocol {
+    // tcp
+    tcp,
+    // udp
+    udp,
+    // icmp
+    icmp,
+    // gre
+    gre,
+    // all
+    all;
+}
